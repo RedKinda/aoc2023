6045d1a325044a4442b9019fab2ca15309dff4f2
red <red@kalab.sk>
2023-12-04 16:27:47 +0100

    rust 4a fast unsafe

--- src/a04.rs
+++ src/a04.rs
@@ -1,46 +1,45 @@
-
-
 pub fn run(inp: &str) -> i64 {
     let mut res = 0;
 
-    for line in inp.lines() {
-        let mut owned: [bool; 100] = [false; 100];
+    unsafe {
+        for line in inp.lines() {
+            let mut owned: [bool; 100] = [false; 100];
 
-        for i in 0..10 {
-            // we take index 10 + i*3 and that +1 to parse as usize
-            // space is equal to 0
+            for i in 0..10 {
+                // we take index 10 + i*3 and that +1 to parse as usize
+                // space is equal to 0
 
-            let mut val = 0;
-            let first_char = line.as_bytes()[10 + i * 3];
-            if first_char != b' ' {
-                val = (first_char - b'0') * 10;
+                let mut val = 0;
+                let first_char = line.as_bytes().get_unchecked(10 + i * 3);
+                if *first_char != b' ' {
+                    val = (first_char - b'0') * 10;
+                }
+                let second_char = line.as_bytes().get_unchecked(10 + i * 3 + 1);
+                val += second_char - b'0';
+
+                *owned.get_unchecked_mut(val as usize) = true;
             }
-            let second_char = line.as_bytes()[10 + i * 3 + 1];
-            val += second_char - b'0';
 
-            owned[val as usize] = true;
-        }
+            let mut card_val = 1;
 
-        let mut card_val = 1;
+            for j in 0..25 {
+                let mut val = 0;
+                let first_char = line.as_bytes().get_unchecked(42 + j * 3);
+                if *first_char != b' ' {
+                    val = (first_char - b'0') * 10;
+                }
+                let second_char = line.as_bytes().get_unchecked(42 + j * 3 + 1);
+                val += second_char - b'0';
 
-        for j in 0..25 {
-            let mut val = 0;
-            let first_char = line.as_bytes()[42 + j * 3];
-            if first_char != b' ' {
-                val = (first_char - b'0') * 10;
+                if *owned.get_unchecked(val as usize) {
+                    card_val *= 2;
+                }
             }
-            let second_char = line.as_bytes()[42 + j * 3 + 1];
-            val += second_char - b'0';
 
-            if owned[val as usize] {
-                card_val *= 2;
+            if card_val > 1 {
+                res += card_val / 2;
             }
-        }
-
-        if card_val > 1 {
-            res += card_val / 2;
         }
     }
-
     res
 }
